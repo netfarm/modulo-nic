1ad69bb99f81c56e696249a119e665208f23e449
sherpya <sherpya@netfarm.it>
2013-09-27 03:52:17 +0200

    formattazione

--- modulo-nic.py
+++ modulo-nic.py
@@ -66,8 +66,49 @@ _cf_odd =  [   1, 0, 5, 7, 9, 13, 15, 17, 19, 21,  # 0 - 9
                2, 4, 18, 20, 11, 3, 6, 8, 12, 14, 16, 10, 22, 25, 24, 23 ]
 _cf_even = range(10) + [ 0, 0, 0, 0, 0, 0, 0 ] + range(26)
 
-_province = [u'AG', u'AL', u'AN', u'AO', u'AR', u'AP', u'AT', u'AV', u'BA', u'BT', u'BL', u'BN', u'BG', u'BI', u'BO', u'BZ', u'BS', u'BR', u'CA', u'CL', u'CB', u'CI', u'CE', u'CT', u'CZ', u'CH', u'CO', u'CS', u'CR', u'KR', u'CN', u'EN', u'FM', u'FE', u'FI', u'FG', u'FC', u'FR', u'GE', u'GO', u'GR', u'IM', u'IS', u'SP', u'AQ', u'LT', u'LE', u'LC', u'LI', u'LO', u'LU', u'MC', u'MN', u'MS', u'MT', u'ME', u'MI', u'MO', u'MB', u'NA', u'NO', u'NU', u'OT', u'OR', u'PD', u'PA', u'PR', u'PV', u'PG', u'PU', u'PE', u'PC', u'PI', u'PT', u'PN', u'PZ', u'PO', u'RG', u'RA', u'RC', u'RE', u'RI', u'RN', u'RM', u'RO', u'SA', u'VS', u'SS', u'SV', u'SI', u'SR', u'SO', u'TA', u'TE', u'TR', u'TO', u'OG', u'TP', u'TN', u'TV', u'TS', u'UD', u'VA', u'VE', u'VB', u'VC', u'VR', u'VV', u'VI', u'VT']
-_iso3166_1 = [u'AF', u'AX', u'AL', u'DZ', u'AS', u'AD', u'AO', u'AI', u'AQ', u'AG', u'AR', u'AM', u'AW', u'AU', u'AT', u'AZ', u'BS', u'BH', u'BD', u'BB', u'BY', u'BE', u'BZ', u'BJ', u'BM', u'BT', u'BO', u'BQ', u'BA', u'BW', u'BV', u'BR', u'IO', u'BN', u'BG', u'BF', u'BI', u'KH', u'CM', u'CA', u'CV', u'KY', u'CF', u'TD', u'CL', u'CN', u'CX', u'CC', u'CO', u'KM', u'CG', u'CD', u'CK', u'CR', u'CI', u'HR', u'CU', u'CW', u'CY', u'CZ', u'DK', u'DJ', u'DM', u'DO', u'EC', u'EG', u'SV', u'GQ', u'ER', u'EE', u'ET', u'FK', u'FO', u'FJ', u'FI', u'FR', u'GF', u'PF', u'TF', u'GA', u'GM', u'GE', u'DE', u'GH', u'GI', u'GR', u'GL', u'GD', u'GP', u'GU', u'GT', u'GG', u'GN', u'GW', u'GY', u'HT', u'HM', u'VA', u'HN', u'HK', u'HU', u'IS', u'IN', u'ID', u'IR', u'IQ', u'IE', u'IM', u'IL', u'IT', u'JM', u'JP', u'JE', u'JO', u'KZ', u'KE', u'KI', u'KP', u'KR', u'KW', u'KG', u'LA', u'LV', u'LB', u'LS', u'LR', u'LY', u'LI', u'LT', u'LU', u'MO', u'MK', u'MG', u'MW', u'MY', u'MV', u'ML', u'MT', u'MH', u'MQ', u'MR', u'MU', u'YT', u'MX', u'FM', u'MD', u'MC', u'MN', u'ME', u'MS', u'MA', u'MZ', u'MM', u'NA', u'NR', u'NP', u'NL', u'NC', u'NZ', u'NI', u'NE', u'NG', u'NU', u'NF', u'MP', u'NO', u'OM', u'PK', u'PW', u'PS', u'PA', u'PG', u'PY', u'PE', u'PH', u'PN', u'PL', u'PT', u'PR', u'QA', u'RE', u'RO', u'RU', u'RW', u'BL', u'SH', u'KN', u'LC', u'MF', u'PM', u'VC', u'WS', u'SM', u'ST', u'SA', u'SN', u'RS', u'SC', u'SL', u'SG', u'SX', u'SK', u'SI', u'SB', u'SO', u'ZA', u'GS', u'ES', u'LK', u'SD', u'SR', u'SJ', u'SZ', u'SE', u'CH', u'SY', u'TW', u'TJ', u'TZ', u'TH', u'TL', u'TG', u'TK', u'TO', u'TT', u'TN', u'TR', u'TM', u'TC', u'TV', u'UG', u'UA', u'AE', u'GB', u'US', u'UM', u'UY', u'UZ', u'VU', u'VE', u'VN', u'VG', u'VI', u'WF', u'EH', u'YE', u'ZM', u'ZW']
+_province = [   u'AG', u'AL', u'AN', u'AO', u'AR', u'AP', u'AT', u'AV',
+                u'BA', u'BT', u'BL', u'BN', u'BG', u'BI', u'BO', u'BZ',
+                u'BS', u'BR', u'CA', u'CL', u'CB', u'CI', u'CE', u'CT',
+                u'CZ', u'CH', u'CO', u'CS', u'CR', u'KR', u'CN', u'EN',
+                u'FM', u'FE', u'FI', u'FG', u'FC', u'FR', u'GE', u'GO',
+                u'GR', u'IM', u'IS', u'SP', u'AQ', u'LT', u'LE', u'LC',
+                u'LI', u'LO', u'LU', u'MC', u'MN', u'MS', u'MT', u'ME',
+                u'MI', u'MO', u'MB', u'NA', u'NO', u'NU', u'OT', u'OR',
+                u'PD', u'PA', u'PR', u'PV', u'PG', u'PU', u'PE', u'PC',
+                u'PI', u'PT', u'PN', u'PZ', u'PO', u'RG', u'RA', u'RC',
+                u'RE', u'RI', u'RN', u'RM', u'RO', u'SA', u'VS', u'SS',
+                u'SV', u'SI', u'SR', u'SO', u'TA', u'TE', u'TR', u'TO',
+                u'OG', u'TP', u'TN', u'TV', u'TS', u'UD', u'VA', u'VE',
+                u'VB', u'VC', u'VR', u'VV', u'VI', u'VT']
+
+_iso3166_1 = [  u'AF', u'AX', u'AL', u'DZ', u'AS', u'AD', u'AO', u'AI', u'AQ',
+                u'AG', u'AR', u'AM', u'AW', u'AU', u'AT', u'AZ', u'BS', u'BH',
+                u'BD', u'BB', u'BY', u'BE', u'BZ', u'BJ', u'BM', u'BT', u'BO',
+                u'BQ', u'BA', u'BW', u'BV', u'BR', u'IO', u'BN', u'BG', u'BF',
+                u'BI', u'KH', u'CM', u'CA', u'CV', u'KY', u'CF', u'TD', u'CL',
+                u'CN', u'CX', u'CC', u'CO', u'KM', u'CG', u'CD', u'CK', u'CR',
+                u'CI', u'HR', u'CU', u'CW', u'CY', u'CZ', u'DK', u'DJ', u'DM',
+                u'DO', u'EC', u'EG', u'SV', u'GQ', u'ER', u'EE', u'ET', u'FK',
+                u'FO', u'FJ', u'FI', u'FR', u'GF', u'PF', u'TF', u'GA', u'GM',
+                u'GE', u'DE', u'GH', u'GI', u'GR', u'GL', u'GD', u'GP', u'GU',
+                u'GT', u'GG', u'GN', u'GW', u'GY', u'HT', u'HM', u'VA', u'HN',
+                u'HK', u'HU', u'IS', u'IN', u'ID', u'IR', u'IQ', u'IE', u'IM',
+                u'IL', u'IT', u'JM', u'JP', u'JE', u'JO', u'KZ', u'KE', u'KI',
+                u'KP', u'KR', u'KW', u'KG', u'LA', u'LV', u'LB', u'LS', u'LR',
+                u'LY', u'LI', u'LT', u'LU', u'MO', u'MK', u'MG', u'MW', u'MY',
+                u'MV', u'ML', u'MT', u'MH', u'MQ', u'MR', u'MU', u'YT', u'MX',
+                u'FM', u'MD', u'MC', u'MN', u'ME', u'MS', u'MA', u'MZ', u'MM',
+                u'NA', u'NR', u'NP', u'NL', u'NC', u'NZ', u'NI', u'NE', u'NG',
+                u'NU', u'NF', u'MP', u'NO', u'OM', u'PK', u'PW', u'PS', u'PA',
+                u'PG', u'PY', u'PE', u'PH', u'PN', u'PL', u'PT', u'PR', u'QA',
+                u'RE', u'RO', u'RU', u'RW', u'BL', u'SH', u'KN', u'LC', u'MF',
+                u'PM', u'VC', u'WS', u'SM', u'ST', u'SA', u'SN', u'RS', u'SC',
+                u'SL', u'SG', u'SX', u'SK', u'SI', u'SB', u'SO', u'ZA', u'GS',
+                u'ES', u'LK', u'SD', u'SR', u'SJ', u'SZ', u'SE', u'CH', u'SY',
+                u'TW', u'TJ', u'TZ', u'TH', u'TL', u'TG', u'TK', u'TO', u'TT',
+                u'TN', u'TR', u'TM', u'TC', u'TV', u'UG', u'UA', u'AE', u'GB',
+                u'US', u'UM', u'UY', u'UZ', u'VU', u'VE', u'VN', u'VG', u'VI',
+                u'WF', u'EH', u'YE', u'ZM', u'ZW']
 
 class validators:
     @staticmethod
